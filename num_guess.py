9c85938e2e92ecd17d98c3bedb4b08245a312203
Ken Hileman <ken.hileman@outlook.com>
2024-04-11 20:19:38 -0400

    Assignment 3c First Commit

---
+++ num_guess.py
@@ -0,0 +1,26 @@
+# Author: Kenneth Hileman
+# GitHub username: PHTIWringer
+# Date: 04/09/2024
+# Description: Asks the user to enter an integer that a player will guess. Display whether the guess is too high or too low or displays that the player got it right in x amount of times.
+
+num = int(input("Enter the integer for the player to guess."))
+print(num)
+
+guess = int(input("Enter your guess."))
+print(guess)
+
+num_guesses = 1
+
+while True:
+
+    if guess == num:
+        print("You guessed it in", num_guesses, "tries.")  
+        break
+    elif guess > num:
+        print("Too high - try again:")
+    else:    
+        print("Too low - try again:")
+
+    guess = int(input())
+    num_guesses += 1
+        
